b870a657615951baf6c51a37a64882fa3c7761ae
bksoftwarevn <bksoftwarevn@gmail.com>
2021-01-07 20:36:51 +0700

    Bai 4 - Thuc hanh if else

---
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfElsePractice.java
@@ -0,0 +1,16 @@
+package com.bksoftwarevn.bai4_condition;
+
+public class IfElsePractice {
+    /*
+    Xay dung chuong trinh nhap vao cac thong tin sau tu ban phim
+    1. Ho va ten
+    2. Ngay sinh
+    3. Thang sinh
+    4. Nam sinh
+    5. Ngay, thang, nam hien tai
+
+    Kiem tra xem nam sinh va nam hien tai, nam nao la nam khong nhuan?
+    Ngay hom nay la ngay thu bao nhieu trong nam?
+    *Kiem tra xem ngay ban sinh ra la thu may trong tuan?
+     */
+}
