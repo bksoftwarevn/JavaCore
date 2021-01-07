3134c60cb7eb0c8e74af10e6b874e5ea80905816
bksoftwarevn <bksoftwarevn@gmail.com>
2021-01-07 20:15:25 +0700

    Bai 4 Cau Lenh If

---
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfDemo.java
@@ -0,0 +1,17 @@
+package com.bksoftwarevn.bai4_condition;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        /*
+            if(dk){
+                statements;
+            }
+            Kiem tra dk neu dung thi thuc hien cac cau lenh statements trong block if
+         */
+
+        int a = 10;
+        if (a % 2 == 0) {
+            System.out.println("a la so chan");
+        }
+    }
+}
