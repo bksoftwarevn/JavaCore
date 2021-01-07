f8967659ac14aba2d60940456dc4776a9fc658a6
bksoftwarevn <bksoftwarevn@gmail.com>
2021-01-07 22:01:41 +0700

    Update bai tap

--- bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfElsePractice.java
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfElsePractice.java
@@ -12,5 +12,6 @@ public class IfElsePractice {
     Kiem tra xem nam sinh va nam hien tai, nam nao la nam khong nhuan?
     Ngay hom nay la ngay thu bao nhieu trong nam?
     *Kiem tra xem ngay ban sinh ra la thu may trong tuan?
+    * Nam nay co bao nhieu ngay la thu 6 va la ngay 13
      */
 }
--- bkes-java-core/src/com/bksoftwarevn/bai4_condition/manhnd/ManhND.java
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/manhnd/ManhND.java
@@ -1,7 +1,55 @@
 package com.bksoftwarevn.bai4_condition.manhnd;
 
+import java.util.*;
+
 public class ManhND {
     public static void main(String[] args) {
-        //abcde
+        String name;
+        int myBirthday;
+        int myMonthDay;
+        int myYear;
+
+        int today;
+        int month;
+        int year;
+
+        System.out.println("Nhap ten:");
+        name = new Scanner(System.in).nextLine();
+        System.out.println("Nhap ngay sinh:");
+        myBirthday = new Scanner(System.in).nextInt();
+        System.out.println("Nhap thang sinh:");
+        myMonthDay = new Scanner(System.in).nextInt();
+        System.out.println("Nhap nam sinh:");
+        myYear = new Scanner(System.in).nextInt();
+
+
+
+        System.out.println("Nhap ngay:");
+        today = new Scanner(System.in).nextInt();
+        System.out.println("Nhap thang:");
+        month = new Scanner(System.in).nextInt();
+        System.out.println("Nhap nam:");
+        year = new Scanner(System.in).nextInt();
+
+        System.out.println("Dang kiem tra nam khong nhuan....");
+
+        if(!((myYear%4==0 && myYear%100!=0) || myYear%400==0)){
+            System.out.println("Nam sinh khong la nam nhuan!");
+        }
+
+        if(!((year%4==0 && year%100!=0) || year%400==0)){
+            System.out.println("Nam hien tai khong la nam nhuan!");
+        }
+
+        System.out.println("Hoan thanh kiem tra!");
+
+
+
+
+
+
+
     }
+
+
 }
