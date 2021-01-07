04fde043aac960872688401bb5a1d4d1e2328f77
bksoftwarevn <bksoftwarevn@gmail.com>
2021-01-07 20:21:44 +0700

    Bai 4 Cau Lenh If-else

--- bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfDemo.java
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/IfDemo.java
@@ -13,5 +13,34 @@ public class IfDemo {
         if (a % 2 == 0) {
             System.out.println("a la so chan");
         }
+
+        /*
+            if(dk){
+                statements1;
+            }else{
+                statements2;
+            }
+            Kiem tra dk neu dung thi thuc hien cac cau lenh statements1 trong block if
+            Nguoc lai thuc hien cac lenh statements2 trong block else
+         */
+
+        if (a < 10) {
+            System.out.println("a nho hon 10");
+        } else {
+            System.out.println("a lon hon 10");
+        }
+
+        a = 100;
+        if (a < 10) {
+            System.out.println("a nho hon 10");
+            System.out.println("Khong co block code");
+        }
+        // Truong hop khong co block code {}
+        if (a < 10)
+            System.out.println("a nho hon 10");
+
+        System.out.println("Khong co block code");
+
+
     }
 }
