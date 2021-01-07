6c171f6b91dbfd91a1c8a45318e55c4e0d7ec060
bksoftwarevn <bksoftwarevn@gmail.com>
2021-01-07 20:09:22 +0700

    Bai 3 wrapper class - done

--- bkes-java-core/src/com/bksoftwarevn/bai3_wrapper_class/WrapperClass.java
+++ bkes-java-core/src/com/bksoftwarevn/bai3_wrapper_class/WrapperClass.java
@@ -4,6 +4,8 @@ public class WrapperClass {
     public static void main(String[] args) {
         int a = 10;
         Integer b = 10;
+        int c = 10;
+        Integer d = new Integer(10);
 
         System.out.println("a + 1 = "  + (a+1));
         System.out.println("b + 1 = "  + (b+1));
@@ -11,5 +13,13 @@ public class WrapperClass {
         System.out.println(b.doubleValue() + 1); // 11.0
         System.out.println(b.toString() + 1); // 101
         System.out.println(b.intValue() + 1); // 11
+
+
+        // autoboxing
+        Integer m = 10;
+
+        //unboxing
+        int n = m;  // Ban chat la no goi cau lenh intValue() de tra ve int -> giong lenh duoi
+        int f = m.intValue();
     }
 }
