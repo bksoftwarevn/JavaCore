9e63558123d8b722377cf747193149358604eadb
nqsonmb2019 <nqsonmb2019@gmail.com>
2021-02-03 18:10:04 +0700

    Done

--- bkes-java-core/src/com/bksoftwarevn/bai4_condition/sonnq/Demo2.java
+++ bkes-java-core/src/com/bksoftwarevn/bai4_condition/sonnq/Demo2.java
@@ -6,7 +6,7 @@ public class Demo2 {
 
     public static void main(java.lang.String[] args) {
 
-        System.out.println("Mời bạn nhập giá trị ");
+        System.out.println("Mời bạn nhập giá trị vào đây : ");
         int a = new Scanner(System.in).nextInt();
         System.out.println("Giá trị bạn vừa nhập vào là : " + a);
         if (a < 0) {
@@ -15,9 +15,10 @@ public class Demo2 {
         } else if (a == 0) {
 
             System.out.println("Giá trị bên trong tồn tại giá trị = 0!");
+
         } else {
 
-            System.out.println("Giá trị bên trong tồn tại");
+            System.out.println("Giá trị bên trong tồn tại !");
         }
 
 
